c450b6b30974d683fd419086f1ba7bbde2c39995
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-20 18:10:45 -0300

    Inserindo com exec

---
+++ inserir-aluno.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$student = new Student(null, 'Rodrigo de Oliveira', new \DateTimeImmutable('1989-11-13'));
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+
+var_dump($pdo->exec($sqlInsert));
