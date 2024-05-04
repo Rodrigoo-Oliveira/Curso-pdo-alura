e96e506daff1917d3a93c059ed1aca2bd5ed8e76
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-05-04 17:18:35 -0300

    Buscas específicas

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,7 +7,10 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Rodrigo de Oliveira', new \DateTimeImmutable('1989-11-13'));
+$student = new Student(
+    null, 'Rodrigo de Oliveira',
+    new \DateTimeImmutable('1989-11-13')
+);
 
 $sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
 
--- lista-alunos.php
+++ lista-alunos.php
@@ -7,7 +7,24 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
+$statement = $pdo->query('SELECT * FROM students WHERE id=1;');
+
+//Buscas específicas
+/*
 $statement = $pdo->query('SELECT * FROM students;');
+
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+
+    echo $student->age() . PHP_EOL;
+}
+*/
+
+
 $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 $studentList = [];
 
